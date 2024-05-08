21d9bd26c71a20f269d6c8a791bf4bf56ded63fd
WokkcLock <156059451+WokkcLock@users.noreply.github.com>
2024-05-08 11:43:15 +0000

    delete some

--- src/utils/DanbooruApi.ts
+++ src/utils/DanbooruApi.ts
@@ -1,17 +1,19 @@
 import AbstractFetcher from "./Fetcher/Fetcher.js";
 import CurlFetcher from "./Fetcher/CurlFetcher.js";
 import { LogLevel, levelLog } from "./LevelLog.js";
-import fs from 'fs';
 import { asyncSleep } from "./ToolFunc.js";
 import { TagFetchError } from "./User/CustomError.js";
 
-const DanbooruBaseApiUrl = 'https://danbooru.donmai.us/posts.json';
-const getLimit = 20;    // 每次请求限制
+const DanbooruBaseApiUrl = "https://danbooru.donmai.us/posts.json";
+const getLimit = 20; // 每次请求限制
 
 class DanbooruApi {
     private _fetcher: AbstractFetcher;
-    private _authObj: { login: string, api_key: string } | undefined;
-    private _tagsUrlCache = new Map<string, { url: string, ext: string, id: number, source_url: string }[]>();
+    private _authObj: { login: string; api_key: string } | undefined;
+    private _tagsUrlCache = new Map<
+        string,
+        { url: string; ext: string; id: number; source_url: string }[]
+    >();
     private _rating: Rating | undefined;
 
     private constructor(fetcher: AbstractFetcher) {
@@ -25,7 +27,7 @@ class DanbooruApi {
     }
 
     private async getImg(url: string) {
-        return (await this._fetcher.GetByteFile(url));
+        return await this._fetcher.GetByteFile(url);
     }
 
     private async fetchDanbooruApi(params: DanbooruParams) {
@@ -33,7 +35,6 @@ class DanbooruApi {
     }
 
     private async updateTagsMap(tagsWithRating: string) {
-
         const params: DanbooruParams = {
             tags: `${tagsWithRating} random:${getLimit}`,
             // random: true,
@@ -45,20 +46,30 @@ class DanbooruApi {
             params.api_key = this._authObj.api_key;
         }
         levelLog(LogLevel.deploy, `Tags: ${params.tags}, Update map.`);
-        const res = await this.fetchDanbooruApi(params) as { [key: string]: any }[];
-        const dataList = [] as { url: string, ext: string, id: number, source_url: string }[];
+        const res = (await this.fetchDanbooruApi(params)) as {
+            [key: string]: any;
+        }[];
+        const dataList = [] as {
+            url: string;
+            ext: string;
+            id: number;
+            source_url: string;
+        }[];
         for (const item of res) {
             try {
                 dataList.push({
                     id: item.id,
-                    url: item.media_asset.variants[3].url,  // url
+                    url: item.media_asset.variants[3].url, // url
                     ext: item.media_asset.variants[3].file_ext,
-                    source_url: item.file_url      // 源图链接
-                })
+                    source_url: item.file_url, // 源图链接
+                });
             } catch (err) {
                 //* 有可能会失败，但是不管
-                levelLog(LogLevel.warn, `id: ${item.id} media_asset.variants fail.`);   // 一般来说是fail.
-                fs.appendFileSync('Dan_er.json', JSON.stringify(item));
+                levelLog(
+                    LogLevel.warn,
+                    `id: ${item.id} media_asset.variants fail.`,
+                ); // 一般来说是fail.
+                // fs.appendFileSync('Dan_er.json', JSON.stringify(item));
                 continue;
             }
         }
@@ -79,7 +90,10 @@ class DanbooruApi {
                 if (cacheList.length <= 0) {
                     failCount++;
                     if (failCount >= 2) {
-                        levelLog(LogLevel.error, `update tags:${tags}, meet the fail limit.`);
+                        levelLog(
+                            LogLevel.error,
+                            `update tags:${tags}, meet the fail limit.`,
+                        );
                         throw new TagFetchError(tags);
                     }
                 } else {
@@ -91,7 +105,6 @@ class DanbooruApi {
             }
         }
 
-
         const ret = cacheList.pop()!;
         return ret;
     }
@@ -99,13 +112,12 @@ class DanbooruApi {
     EnableAuth(name: string, apiKey: string) {
         this._authObj = {
             login: name,
-            api_key: apiKey
+            api_key: apiKey,
         };
     }
 
     DisableAuth() {
-        if (this._authObj == undefined)
-            return;
+        if (this._authObj == undefined) return;
         this._authObj = undefined;
     }
 
@@ -115,14 +127,14 @@ class DanbooruApi {
 
     GetRating() {
         switch (this._rating) {
-            case 'g':
-                return 'general';
-            case 's':
-                return 'sensitive';
-            case 'q':
-                return 'questionable';
-            case 'e':
-                return  'explicit';      
+            case "g":
+                return "general";
+            case "s":
+                return "sensitive";
+            case "q":
+                return "questionable";
+            case "e":
+                return "explicit";
         }
         return undefined;
     }
@@ -138,14 +150,14 @@ class DanbooruApi {
             ext: ret.ext,
             id: ret.id,
             source_url: ret.source_url,
-        }
+        };
     }
 
     async GetImageFromId(id: number) {
-        const res = await this.fetchDanbooruApi({
+        const res = (await this.fetchDanbooruApi({
             page: `b${id + 1}`,
             limit: 1,
-        }) as { [key: string]: any }[];
+        })) as { [key: string]: any }[];
 
         try {
             return {
@@ -153,13 +165,11 @@ class DanbooruApi {
                 ext: res[0].media_asset.variants[3].file_ext,
                 id: res[0].id,
                 source_url: res[0].file_url,
-            }
+            };
         } catch {
-            throw new Error('Get image from id.');
+            throw new Error("Get image from id.");
         }
-
     }
-
 }
 
 export default DanbooruApi;
--- src/utils/Fetcher/CurlFetcher.ts
+++ src/utils/Fetcher/CurlFetcher.ts
@@ -8,46 +8,59 @@ class CurlFetcher extends AbstractFetcher {
         super();
     }
 
-    Init() { }
+    Init() {}
 
     private static preFormatUrl(url: string, params: ParsedUrlQueryInput) {
         //* 包上双引号，不让类似括号的特殊符号导致错误
-        return isEmptyObject(params) ? `"${url}"` : `"${url}?${queryString.stringify(params)}"`;
+        return isEmptyObject(params)
+            ? `"${url}"`
+            : `"${url}?${queryString.stringify(params)}"`;
     }
-
 
     async GetJson(url: string, params: ParsedUrlQueryInput = {}): Promise<any> {
         return new Promise((resolve, reject) => {
             //* -s: 不输出错误和进度信息，这主要是为了回避if(stderr)在正常情况也会出错
-            exec(`curl -s ${CurlFetcher.preFormatUrl(url, params)}`, (error, stdout, stderr) => {
-                if (error) {
-                    reject(new Error(`执行 curl 命令时发生错误: ${error.message}`));
-                    return;
-                }
-                if (stderr) {
-                    reject(new Error(stderr));
-                    return;
-                }
-                //* 和cycletls不同, curl请求的数据需要手动反序列化
-                resolve(JSON.parse(stdout));
-            });
+            exec(
+                `curl -s ${CurlFetcher.preFormatUrl(url, params)}`,
+                (error, stdout, stderr) => {
+                    if (error) {
+                        reject(
+                            new Error(
+                                `执行 curl 命令时发生错误: ${error.message}`,
+                            ),
+                        );
+                        return;
+                    }
+                    if (stderr) {
+                        reject(new Error(stderr));
+                        return;
+                    }
+                    //* 和cycletls不同, curl请求的数据需要手动反序列化
+                    resolve(JSON.parse(stdout));
+                },
+            );
         });
     }
 
-    async GetByteFile(url: string, params: ParsedUrlQueryInput = {}): Promise<Buffer> {
+    async GetByteFile(
+        url: string,
+        params: ParsedUrlQueryInput = {},
+    ): Promise<Buffer> {
         return new Promise((resolve, reject) => {
             //* -s: 不输出错误和进度信息，这主要是为了回避if(stderr)在正常情况也会出错
             const command = `curl -s ${CurlFetcher.preFormatUrl(url, params)}`;
-            exec(command, { encoding: 'binary' }, (error, stdout, stderr) => {
+            exec(command, { encoding: "binary" }, (error, stdout, stderr) => {
                 if (error) {
-                    reject(new Error(`执行 curl 命令时发生错误: ${error.message}`));
+                    reject(
+                        new Error(`执行 curl 命令时发生错误: ${error.message}`),
+                    );
                     return;
                 }
                 if (stderr) {
                     reject(new Error(stderr));
                     return;
                 }
-                resolve(Buffer.from(stdout, 'binary'));
+                resolve(Buffer.from(stdout, "binary"));
             });
         });
     }
